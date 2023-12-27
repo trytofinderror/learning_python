8a952b3cd0fcf32cd76b3975aa63f238629ec744
trytofinderror <trytofinderror@gmail.com>
2023-12-27 22:06:51 +0300

    Урок 1. Числовые типы данных (аннотация переменных)

--- Lesson 1 (Numeric)/spent_calories.py
+++ Lesson 1 (Numeric)/spent_calories.py
@@ -1,11 +1,12 @@
 from decimal import Decimal
 
+
 def spent_calories(
-        weight=75,
-        height=175,
-        dist=9.75,
-        hours=2,
-        minutes=120
+        weight: float = 75,
+        height: float = 175,
+        dist: float = 9.75,
+        hours: float = 2,
+        minutes: float = 120
 ):
     return Decimal((0.035 * weight + (dist / hours) ** 2 / height * 0.029 * weight) * minutes)
 
